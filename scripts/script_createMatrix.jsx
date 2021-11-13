56db937f938949c63e76026a75aeed76f22c5d40
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2021-11-13 15:10:43 +0200

    Update script_createMatrix.jsx

--- scripts/script_createMatrix.jsx
+++ scripts/script_createMatrix.jsx
@@ -4,22 +4,23 @@
 
 /*
 How to:
- Select objects in Illustrator
- Order of objects (from top): 
+ Select following objects in Illustrator (order matters; first element of the list is on top): 
     Textbox:   Name and title(s) of a person, on two lines
     Rectangle: The back
     Textbox:   Section title
     Rectangle: From which the photo is masked.
+ Run code!
 */
 
+// Set the number of columns
 var columns = 7 // 12 in 2018 - 14 in 2019 - 13 in 2020
 
-// Replaces left text with the one on the right. 
+// Replaces left text with the one on the right. It is applied everyhere on the matrix.
 var partialText = {
     "Todennäköisyyslaskenta" : "Todennäköisyys-\nlaskenta",
     };
 
-// Replaces text if the text is a perfect match
+// Same as above, but replaces text if the text is a perfect match
 var wholeText = {
     };
 
@@ -31,6 +32,3 @@ var options = {
     };
 
 createTable(columns, options, partialText, wholeText);
-
-// Ohjelman kaatuessa / kaikkea dataa ei ole saatavilla.
-// 140 x 210
